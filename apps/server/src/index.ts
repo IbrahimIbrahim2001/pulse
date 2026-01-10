e1a3aa715f27aeb8378a3193dab3ec6315b6e879
IbrahimIbrahim2001 <ii8538098@gmail.com>
2026-01-10 20:12:34 +0300

    cookies problem

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -20,7 +20,8 @@ app.use(
 	cors({
 		origin: process.env.CORS_ORIGIN || "",
 		allowMethods: ["GET", "POST", "OPTIONS"],
-		allowHeaders: ["Content-Type", "Authorization"],
+		allowHeaders: ["Content-Type", "Authorization", "Cookies"],
+		exposeHeaders: ["Set-Cookie"],
 		credentials: true,
 	}),
 );
